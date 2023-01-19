c782cc42b35ddb4b4524f14b1958d54ecd0f655f
Kirill Morozov <morozov.ks@gmail.com>
2023-01-20 00:46:18 +0300

    refact: refact layout main

--- src/components/Movies/MoviesCard/MoviesCard.jsx
+++ src/components/Movies/MoviesCard/MoviesCard.jsx
@@ -1,50 +1,27 @@
 import './MoviesCard.css';
 import { useContext } from 'react';
 
-export const MoviesCard = ({
-                         onCardClick, onCardLike, onCardDelete, movie, onTrashClick,
+export const MoviesCard = ({movie,
                      }) => {
-    // const currentUserContext = useContext(CurrentUserContext);
-    //
-    // const isOwn = card.owner === currentUserContext._id;
-    // const cardDeleteButtonClassName = (
-    //     `elements__trash-button ${isOwn ? 'elements__trash-button_visible' : ''}`
-    // );
-    //
-    // const isLiked = card.likes.some((item) => item === currentUserContext._id);
-    // const cardLikeButtonClassName = (
-    //     `elements__like-button ${isLiked ? 'elements_like-button_active' : ''}`
-    // );
-    //
-    // function handleClick() {
-    //     onCardClick(card);
-    // }
-    //
-    // function handleLikeClick() {
-    //     onCardLike(card);
-    // }
-    //
-    // function handleDeleteClick() {
-    //     onTrashClick(true);
-    //
-    //     onCardDelete(card);
-    // }
+
+    const convertMinutesToHours = (minutes) => {
+        const hours = Math.floor(minutes / 60);
+        const remainingMinutes = minutes % 60;
+        return `${hours}ч ${remainingMinutes}м`;
+    }
 
     return (
         <article className="moviescard">
             <img className="moviescard__image" src={movie.image} alt={movie.nameRU} />
             <div className="moviescard__text">
                 <h2 className="moviecard__title">{movie.nameRU}</h2>
-                <p className="moviescard__duration">{movie.duration}</p>
+                <p className="moviescard__duration">{convertMinutesToHours(movie.duration)}</p>
                 <button
                     type="button"
                     className="moviescard__like-button"
-                    // onClick={() => handleLikeClick()}
                     aria-label="Понравилось"
                 />
-                {/*<span className="elements__like-counter">{card.likes.length}</span>*/}
             </div>
-            {/*<button type="button" className={cardDeleteButtonClassName} onClick={() => handleDeleteClick()} aria-label="Корзина" />*/}
         </article>
     );
 };
