0eddd0b74d338f398441069c9c1a6ddd417c503a
Kirill Morozov <morozov.ks@gmail.com>
2023-01-27 00:08:57 +0300

    feat: has more button logic

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,74 +2,8 @@ import './Movies.css';
 import {SearchForm} from "./SearchForm/SearchForm";
 import {MoviesCardList} from "./MoviesCardList/MoviesCardList";
 import Preloader from "../Preloader/Preloader";
-import {useEffect, useState} from "react";
 
 export const Movies = ({isLoaded, onSearch, movies, searchRequest, handleSaveMovies, searchError }) => {
-    const [index, setIndex] = useState({
-        start: 12,
-        load: 3,
-    });
-    const [loadMore, setLoadMore] = useState(true);
-
-    const handleLoadMore = () => {
-        if (movies.length <= index.start) {
-            setLoadMore(!loadMore)
-        }
-    }
-
-    const handleRenderCounter = (movies) => {
-        if (window.innerWidth > 1196) {
-            setIndex({
-                start: 12,
-                load: 3,
-            })
-        }
-
-        if (window.innerWidth < 1200) {
-            setIndex({
-                start: 8,
-                load: 2,
-            })
-        }
-
-        if (window.innerWidth < 767) {
-            setIndex({
-                start: 5,
-                load: 1,
-            })
-        }
-    };
-
-    const itemsToRender = movies.slice(0, index.start);
-
-    const handleHasMore = () => {
-        setIndex({
-            start: index.start + index.load,
-            load: index.load,
-        })
-    }
-
-    // useEffect(() => {
-    //     const handleWindowLoad = () => {
-    //             if (movies.length <= index.start) {
-    //                 setLoadMore(!loadMore)
-    //             }
-    //     };
-    //
-    //     window.addEventListener('load', handleWindowLoad);
-    //
-    //     return () => window.removeEventListener('load', handleWindowLoad);
-    // }, [movies.length])
-
-    useEffect(() => {
-        window.addEventListener("resize", handleRenderCounter);
-
-        return () => window.removeEventListener("resize", handleRenderCounter);
-    }, []);
-
-    // console.log(movies.length)
-    // console.log(loadMore)
-
 
     return (
         <section className="movies">
@@ -78,10 +12,10 @@ export const Movies = ({isLoaded, onSearch, movies, searchRequest, handleSaveMov
                 onSearch={onSearch}
             />
             {isLoaded ? <MoviesCardList
-                        onAddMore={handleHasMore}
+                        // onAddMore={handleHasMore}
                         onSave={handleSaveMovies}
-                        movies={itemsToRender}
-                        loadMore={loadMore}
+                        movies={movies}
+                        // loadMore={loadMore}
                         searchError={searchError}
                     /> : <Preloader />}
         </section>
--- src/components/Movies/MoviesCardList/MoviesCardList.jsx
+++ src/components/Movies/MoviesCardList/MoviesCardList.jsx
@@ -1,16 +1,71 @@
 import './MoviesCardList.css';
 import {MoviesCard} from "../MoviesCard/MoviesCard";
 import {useLocation} from "react-router-dom";
+import {useEffect, useState} from "react";
 
-export const MoviesCardList = ({movies, onSave, onAddMore, loadMore, searchError }) => {
+export const MoviesCardList = ({movies, onSave, searchError }) => {
     const location = useLocation();
+
+    const [index, setIndex] = useState({
+        start: 12,
+        load: 3,
+    });
+    const [loadMore, setLoadMore] = useState(true);
+
+    const handleRenderCounter = (movies) => {
+        if (window.innerWidth > 1196) {
+            setIndex({
+                start: 12,
+                load: 3,
+            })
+        }
+
+        if (window.innerWidth < 1200) {
+            setIndex({
+                start: 8,
+                load: 2,
+            })
+        }
+
+        if (window.innerWidth < 767) {
+            setIndex({
+                start: 5,
+                load: 1,
+            })
+        }
+    };
+
+    const renderMovies = movies.slice(0, index.start);
+
+    const handleHasMore = () => {
+        setIndex({
+            start: index.start + index.load,
+            load: index.load,
+        })
+
+        if (renderMovies.length >= movies.length - index.load) {
+            setLoadMore(false)
+        }
+    }
+
+    useEffect(() => {
+        if (movies.length <= index.load) {
+            setLoadMore(false)
+        } else {
+            setLoadMore(true)
+        }
+
+        window.addEventListener("resize", handleRenderCounter);
+
+        return () => window.removeEventListener("resize", handleRenderCounter);
+    }, [movies, index.load]);
 
     return (
         <section className="movieslist">
             {location.pathname === '/movies' && !searchError && (
                 <>
                     <ul className="moviescardlist">
-                        {movies.map((movie) => (
+                        {renderMovies.map((movie) => (
                             <MoviesCard
                                 onSave={onSave}
                                 key={(movie.id)}
@@ -19,7 +74,7 @@ export const MoviesCardList = ({movies, onSave, onAddMore, loadMore, searchError
                         ))}
                     </ul>
                     {loadMore && <button
-                        onClick={onAddMore}
+                        onClick={handleHasMore}
                         className="movieslist__more-button">
                         Ещё
                     </button>}
